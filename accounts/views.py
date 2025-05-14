f343822ac2df8d5db4f0b906b36730be4c51d2cf
seika okamoto <kamesei1224@gmail.com>
2025-05-15 06:18:09 +0900

    ログアウト機能追加・ログイン状態表示

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth import logout
 
 def login_view(request):
     if request.method == 'POST':
@@ -16,3 +17,6 @@ def login_view(request):
     
     return render(request, 'accounts/login.html')
 
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')  # ログアウト後にログイン画面に戻す
