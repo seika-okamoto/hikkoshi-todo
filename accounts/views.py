1d94cd5f636863758865ca7491d3d6220245c666
seika okamoto <kamesei1224@gmail.com>
2025-05-14 13:59:56 +0900

    ログイン機能完成・リダイレクト修正

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')  # ユーザー名
+        password = request.POST.get('password')  # パスワード
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('todo:index')  # 成功したらtodoアプリのトップへ
+        else:
+            messages.error(request, 'ユーザー名またはパスワードが間違っています')
+    
     return render(request, 'accounts/login.html')
 
