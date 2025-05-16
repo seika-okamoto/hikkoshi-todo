c2c947dec0ccb45c3f53373a2fc40d1504bad496
seika okamoto <kamesei1224@gmail.com>
2025-05-17 06:39:40 +0900

    テンプレート構成を整理して統一、ホーム画面の作成

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from home import views as home_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
     path('accounts/', include('accounts.urls')), 
+    path('home/', home_views.index, name='home'),
 ]
