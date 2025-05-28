119a053de224cdab4fccd5b429636c6eedd80364
seika okamoto <kamesei1224@gmail.com>
2025-05-29 05:42:54 +0900

    ホーム画面の基礎デザインを作成、進捗バーの表示部分など作成中

--- home/views.py
+++ home/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from todo.models import Task  # ← Taskモデルをインポート
 
+@login_required
 def index(request):
-    return render(request, 'home/index.html')
-# Create your views here.
+    tasks = Task.objects.filter(user=request.user)
+    total_tasks = tasks.count()
+    completed_tasks = tasks.filter(is_done=True).count()
+    percent = round((completed_tasks / total_tasks) * 100, 1) if total_tasks > 0 else 0
+    
+    
+    return render(request, 'home/index.html', {
+        'total_tasks': total_tasks,
+        'completed_tasks': completed_tasks,
+        'percent': percent,
+    })
+
