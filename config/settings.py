d6ab532d1f065df8aafe874f29dad4d88e99123b
seika okamoto <kamesei1224@gmail.com>
2025-06-10 22:40:07 +0900

    debug変更

--- config/settings.py
+++ config/settings.py
@@ -24,19 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zde7bb=fbmym1ujks^k5x#gyjp$7ofm-cn5q4(v8^1^du0m0tj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'seika1224.pythonanywhere.com']
-
-
-
-print("✅ settings.py 読み込まれた！")
-print("✅ ALLOWED_HOSTS =", ALLOWED_HOSTS)
-
-
-
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', ['seika1224.pythonanywhere.com'])
 
 
 # Application definition
