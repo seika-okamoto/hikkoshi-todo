bf2dd371de7f262e59af27ec83309b59599168d4
seika okamoto <kamesei1224@gmail.com>
2025-06-15 00:49:53 +0900

    settingpy 変更

--- config/settings.py
+++ config/settings.py
@@ -15,10 +15,9 @@ SECRET_KEY = 'django-insecure-zde7bb=fbmym1ujks^k5x#gyjp$7ofm-cn5q4(v8^1^du0m0tj
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['seika1224.pythonanywhere.com']
-
 
 
 # Application definition
